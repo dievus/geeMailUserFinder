48948d06424f96bb46d18bea62bf9b9f02edc0d6
Joe Helle <25853389+dievus@users.noreply.github.com>
2022-05-07 09:25:07 -0400

    Added GSuite Identification
    - Identifies GSuite now
    - Added a function to work as a command handler
    - Added exception handling for when a file is invalid

--- geeMailUserFinder.py
+++ geeMailUserFinder.py
@@ -52,18 +52,31 @@ python3 geeMailUserFinder.py -r emails.txt -v\n
         '-t', '--timeout', help='Set timeout between checks.')
     opt_parser.add_argument(
         '-v', '--verbose', help='Verbose mode', action='store_true')
+    opt_parser.add_argument('-u', '--userlist', help='Reads usernames from file')
+    opt_parser.add_argument('-d', '--domain', help='Uses manually entered domain.')
+    opt_parser.add_argument('-c', '--checkdomain', help='Checks if domain is valid.', action='store_true')
     global args
     args = opt_parser.parse_args()
     if len(sys.argv) == 1:
         opt_parser.print_help()
         opt_parser.exit()
 
+def handler():
+    if args.email is not None:
+        single_test()
+    if args.read is not None:
+        gmail_test()
+    if args.userlist is not None:
+        gmail_users()
+    if args.checkdomain is not None and args.email is None and args.read is None and args.userlist is None:
+        domain_test()
+
 
 def single_test():
     if args.email is not None:
         email = args.email
         url = "https://mail.google.com/mail/gxlu?email=" + email + "&zx=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-        request = request_handler.get(url)
+        request = request_handler.head(url)
         response = request.text
         cook_check = str(request.cookies.get_dict())
         valid_response = re.search("'COMPASS':", cook_check)
@@ -77,7 +90,6 @@ def single_test():
         pass
 
 def gmail_test():
-
     # counter = 0
     if args.read is not None:  
         with open(args.read, 'r') as line_count:
@@ -88,6 +100,51 @@ def gmail_test():
             for line in f:
                 email = line.split()
                 email = ' '.join(email)
+                url = "https://mail.google.com/mail/gxlu?email=" + email + "&zx=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+                request = request_handler.get(url)
+                cook_check = str(request.cookies.get_dict())
+                valid_response = re.search("'COMPASS':", cook_check)
+                if args.verbose:
+                    print('\n',email, url, request, request.status_code)
+                if valid_response:
+                    b = " Result -   Valid Email Found! [+]"
+                    print(success + f'[+] {email:53} {b}' + Style.RESET_ALL)
+                    counter = counter + 1
+                    if args.write is not None:
+                        with open(args.write, 'a') as valid_emails:
+                            valid_emails.write(f"{email}\n")
+                else:
+                    b = " Result - Invalid Email Found! [-]"
+                    print(fail + f'[-] {email:53} {b}' + Style.RESET_ALL)
+                if args.timeout is not None:
+                    time.sleep(int(args.timeout))
+            if counter == 0:
+                print(
+                    fail + '\n[-] There were no valid logins found. [-]' + close)
+            if counter == 1:
+                print(
+                    info + '\n[info] geeMail User Finder discovered one valid login account.' + close)
+            if counter > 1:
+                print(
+                    info + f'\n[info] geeMail User Finder discovered {counter} valid login accounts.' + close)
+
+    else:
+            pass
+
+def gmail_users():
+    # counter = 0
+    print(info + f'\n [!] Checking if target domain uses GSuite.')
+    domain_test()
+    if args.userlist is not None:  
+        with open(args.userlist, 'r') as line_count:
+            lines = len(line_count.readlines())
+            print(info + f'\n[!] Loading {lines} usernames to test.\n')
+        with open(args.userlist, 'r') as f:
+            counter = 0
+            for line in f:
+                username = line.split()
+                username = ' '.join(username)
+                email = username + '@' + args.domain
                 url = "https://mail.google.com/mail/gxlu?email=" + email + "&zx=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
                 request = request_handler.get(url)
                 cook_check = str(request.cookies.get_dict())
@@ -119,6 +176,18 @@ def gmail_test():
     else:
             pass
 
+def domain_test():
+    domain = args.domain
+    url = f'https://www.google.com/a/{domain}/ServiceLogin?https://docs.google.com/a/{domain}'
+    request = request_handler.get(url)
+    re.search("Server error", request.text)
+    if args.verbose:
+        print(request.text)    
+    if re.search("Server error", request.text):
+        print(fail + f'\n[-] {domain} - Invalid GSuite domain!')
+    else:
+        print(success + f'\n[+] {domain} - Valid GSuite domain!')
+
 if __name__ == '__main__':
     try:
         init()
@@ -127,8 +196,10 @@ if __name__ == '__main__':
         options()
         print(Fore.YELLOW + Style.BRIGHT +
             f'\n[info] Starting geeMail User Finder at {time.ctime()}' + Style.RESET_ALL)    
-        single_test()
-        gmail_test()
+        handler()
+        # single_test()
+        # gmail_test()
+        # gmail_users()
         print(
             info + f'\n[info] Scan completed at {time.ctime()}\n' + close)
     except KeyboardInterrupt:
@@ -136,3 +207,5 @@ if __name__ == '__main__':
         print(
             info + f'\n[info] Scan completed at {time.ctime()}\n' + close)
         sys.exit()
+    except FileNotFoundError:
+        print(fail + '\n[-] File not found. Check filename and try again [-]' + close)
